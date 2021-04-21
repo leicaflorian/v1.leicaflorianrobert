4b88b826b9fbf4089c56ec978ec5e6fcc372d3ac
Leica Florian <florian.leica@gmail.com>
2021-04-22 00:34:54 +0200

    Adds firebase deploy config and a simple landing page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Leica Florian Robert logo" src="./assets/logo_white.svg" class="main-logo">
+
+  <h1>Coming soon . . .</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "./assets/vonique64-font.css";
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100%;
+  height: 100%;
+  font-family: Vonique64, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background: rgb(60, 99, 119);
+  background: linear-gradient(132deg, rgba(60, 99, 119, 1) 0%, rgba(89, 103, 113, 1) 40%, rgba(69, 100, 117, 1) 80%, rgba(60, 99, 119, 1) 100%);
+}
+
+.main-logo {
+  width: 80%;
+  height: 80%;
+  max-width: 400px;
+  max-height: 400px;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <!-- The core Firebase JS SDK is always required and must be listed first -->
+  <script src="/__/firebase/8.4.1/firebase-app.js"></script>
+
+  <!-- TODO: Add SDKs for Firebase products that you want to use
+       https://firebase.google.com/docs/web/setup#available-libraries -->
+  <script src="/__/firebase/8.4.1/firebase-analytics.js"></script>
+
+  <!-- Initialize Firebase -->
+  <script src="/__/firebase/init.js"></script>
+</template>
+
+<script>
+import Kute from "kute.js"
+
+
+export default {
+  name: 'Home',
+  mounted() {
+
+    const line1 = Kute.allFromTo("#l1",
+        {draw: "0% 0%"},
+        {draw: "0% 100%"},
+        {duration: 500}
+    )
+    const line2 = Kute.allFromTo("#l2-1, #l2-2",
+        {draw: "0% 0%"},
+        {draw: "0% 100%"},
+        {delay: 500, prepareStart: true}
+    )
+
+    line2.start()
+    line1.start()
+
+
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%
+}
+
+.top-container {
+  height: 20vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .top-container-hr {
+    width: 100%;
+    max-width: 600px;
+  }
+}
+
+
+.line {
+  background-color: #fff;
+}
+
+.line-vertical {
+  width: 4px;
+  height: 100%;
+}
+
+.line-horizontal {
+  height: 4px;
+  width: 100%;
+
+}
+</style>
